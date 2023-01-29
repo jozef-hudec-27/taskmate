89485c8150f972c7552d013b8459a382e3942202
Jozef Hudec <jojohudec27@gmail.com>
2023-01-29 12:21:40 +0100

    Create simple Todo class + install date-fns

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './assets/styles/style.scss'
+import * as dateFns from 'date-fns';
 
-console.log('hello from index.js!')
+class Todo {
+  isFinished = false
+
+  constructor(title, description = '', dueDate = new Date(), priority = 1) {
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+  }
+
+  timeLeft() {
+    return dateFns.formatDistanceToNow(this.dueDate)
+  }
+}
+
