6677d74d77f8a485314e52c488161875ded39362
Jozef Hudec <jojohudec27@gmail.com>
2023-01-29 16:24:18 +0100

    Create Project class + add some methods to Todo

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import * as dateFns from 'date-fns';
 class Todo {
   isFinished = false
 
-  constructor(title, description = '', dueDate = new Date(), priority = 1) {
+  constructor(title, project, description = '', dueDate = new Date(), priority = 1) {
     this.title = title
+    this.setProjectTo(project)
     this.description = description
     this.dueDate = dueDate
     this.priority = priority
@@ -14,5 +15,67 @@ class Todo {
   timeLeft() {
     return dateFns.formatDistanceToNow(this.dueDate)
   }
+
+  toggleFinished() {
+    this.isFinished = !this.finished
+  }
+
+  delete() {
+    this.project.removeTodo(this)
+
+    for (let property in this) {
+      delete this[property]
+    }
+  }
+
+  setProjectTo(project) {
+    this.project = project
+    project.addTodo(this)
+  }
 }
 
+class Project {
+  todos = []
+
+  constructor(name) {
+    this.name = name
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo)
+  }
+
+  removeTodo(todo) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i] === todo) {
+        this.todos.splice(i, 1)
+        return true
+      }
+    }
+  }
+
+  delete() {
+    if (this.isDefault) {
+      return
+    }
+
+    this.todos.forEach(todo => {
+      todo.setProjectTo(defaultProject)
+    })
+
+    for (let property in this) {
+      delete this[property]
+    }
+  }
+}
+
+let defaultProject = new Project('default')
+defaultProject.isDefault = true
+
+let secondProject = new Project('second')
+
+let todo = new Todo('good todo', secondProject)
+
+secondProject.delete()
+
+console.log(todo, defaultProject, secondProject)
