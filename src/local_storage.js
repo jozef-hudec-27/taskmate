0adfc83c62da21b8b1e02fd9d7843544e7bb8ff5
Jozef Hudec <jojohudec27@gmail.com>
2023-02-05 11:59:03 +0100

    Clean code + check localstorage browser support

--- src/local_storage.js
+++ src/local_storage.js
@@ -2,19 +2,47 @@ import { TodoProjectAssociation } from "./project"
 
 export class LocalStorage {
   static get(key) {
+    if (!this.isAvailable()) return
+    
     return localStorage.getItem(key)
   }
 
   static set(key, value) {
+    if (!this.isAvailable()) return
+    
     localStorage.setItem(key, value)
   }
 
   static remove(key) {
+    if (!this.isAvailable()) return
+    
     localStorage.removeItem(key)
   }
 
   static isPresent(key) {
+    if (!this.isAvailable()) return
+    
     return this.get(key) !== null
+  }
+
+  static isAvailable() {
+    let storage;
+
+    try {
+      storage = window['localStorage']
+      const x = '__storage_test__'
+      storage.setItem(x, x)
+      storage.removeItem(x)
+      return true
+    }
+    catch (e) {
+      return e instanceof DOMException && (
+          e.code === 22 ||
+          e.code === 1014 ||
+          e.name === 'QuotaExceededError' ||
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          (storage && storage.length !== 0)
+    }
   }
 }
 
@@ -24,12 +52,16 @@ export class LocalStorageProjectService {
   }
 
   static addNewProject(project) {
+    if (!LocalStorage.isAvailable()) return
+
     let currentProjects = this.currentProjects()
     currentProjects.push(project)
     LocalStorage.set('projects', JSON.stringify(currentProjects))
   }
 
   static removeProject(project) {
+    if (!LocalStorage.isAvailable()) return
+
     let currentProjects = this.currentProjects()
 
     for (let i = 0; i < currentProjects.length; i++) {
@@ -43,6 +75,8 @@ export class LocalStorageProjectService {
   }
 
   static addTodoTo(project, todo) {
+    if (!LocalStorage.isAvailable()) return
+
     let currentProjects = this.currentProjects()
 
     for (let i = 0; i < currentProjects.length; i++) {
@@ -58,52 +92,43 @@ export class LocalStorageProjectService {
 
 export class LocalStorageTodoService {
   static toggleFinishedFor(todo) {
-    let stringifiedTodo = JSON.stringify(todo)
-    let project = TodoProjectAssociation.todoToProject[todo.id.toString()]
+    if (!LocalStorage.isAvailable()) return
 
+    let project = TodoProjectAssociation.todoToProject[todo.id.toString()]
     let currentProjects = LocalStorageProjectService.currentProjects()
 
-    for (let i = 0; i < currentProjects.length; i++) {
-      if (currentProjects[i].id == project.id) {
-        for (let j = 0; j < currentProjects[i].todos.length; j++) {
-          if (JSON.stringify(currentProjects[i].todos[j]) === stringifiedTodo) {
-            currentProjects[i].todos[j].isFinished = !currentProjects[i].todos[j].isFinished
-            break
-          }
-        }
-
-        break
-      }
-    }
+    const [i, j] = this.findTodoInTodosOf(project, todo)
+    currentProjects[i].todos[j].isFinished = !currentProjects[i].todos[j].isFinished
 
     LocalStorage.set('projects', JSON.stringify(currentProjects))
   }
 
   static updateDescriptionFor(todo, newDesc) {
-    let stringifiedTodo = JSON.stringify(todo)
-    let project = TodoProjectAssociation.todoToProject[todo.id.toString()]
+    if (!LocalStorage.isAvailable()) return
 
+    let project = TodoProjectAssociation.todoToProject[todo.id.toString()]
     let currentProjects = LocalStorageProjectService.currentProjects()
 
-    for (let i = 0; i < currentProjects.length; i++) {
-      if (currentProjects[i].id == project.id) {
-        for (let j = 0; j < currentProjects[i].todos.length; j++) {
-          if (JSON.stringify(currentProjects[i].todos[j]) === stringifiedTodo) {
-            currentProjects[i].todos[j].description = newDesc
-            break
-          }
-        }
-
-        break
-      }
-    }
+    const [i, j] = this.findTodoInTodosOf(project, todo)
+    currentProjects[i].todos[j].description = newDesc
 
     LocalStorage.set('projects', JSON.stringify(currentProjects))
   }
 
   static removeTodo(todo) {
-    let stringifiedTodo = JSON.stringify(todo)
+    if (!LocalStorage.isAvailable()) return
+
     let project = TodoProjectAssociation.todoToProject[todo.id.toString()]
+    let currentProjects = LocalStorageProjectService.currentProjects()
+
+    const [i, j] = this.findTodoInTodosOf(project, todo)
+    currentProjects[i].todos.splice(j, 1)
+
+    LocalStorage.set('projects', JSON.stringify(currentProjects))
+  }
+
+  static findTodoInTodosOf(project, todo) {
+    let stringifiedTodo = JSON.stringify(todo)
 
     let currentProjects = LocalStorageProjectService.currentProjects()
 
@@ -111,15 +136,10 @@ export class LocalStorageTodoService {
       if (currentProjects[i].id == project.id) {
         for (let j = 0; j < currentProjects[i].todos.length; j++) {
           if (JSON.stringify(currentProjects[i].todos[j]) === stringifiedTodo) {
-            currentProjects[i].todos.splice(j, 1)
-            break
+            return [i, j]
           }
         }
-
-        break
       }
     }
-
-    LocalStorage.set('projects', JSON.stringify(currentProjects))
   }
 }
